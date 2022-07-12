7ce455cfb213fc47c983c9281b36331654780dd0
Bernhard Geisberger <bernhard.geisberger@abusix.com>
2022-07-12 12:49:58 +0200

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,11 @@ tasks.withType(KotlinCompile::class) {
 }
 
 plugins {
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.serialization") version "1.6.10"
-    id("org.jetbrains.dokka") version "1.6.10"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
+    id("org.jetbrains.dokka") version "1.7.0"
     java
-    id("org.gradle.test-retry") version "1.3.1"
+    id("org.gradle.test-retry") version "1.4.0"
     id("com.github.ben-manes.versions") version "0.42.0"
     `maven-publish`
     signing
@@ -24,23 +24,23 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
 
-    implementation("com.google.guava:guava:31.0.1-jre")
-    implementation("org.slf4j:slf4j-api:2.0.0-alpha6")
+    implementation("com.google.guava:guava:31.1-jre")
+    implementation("org.slf4j:slf4j-api:2.0.0-alpha7")
     implementation("org.xerial.snappy:snappy-java:1.1.8.4")
 
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit5"))
     testImplementation(kotlin("reflect"))
-    testImplementation("io.mockk:mockk:1.12.2")
+    testImplementation("io.mockk:mockk:1.12.4")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-    testImplementation("org.testcontainers:testcontainers:1.16.3")
-    testImplementation("com.github.tomakehurst:wiremock:2.27.2")
+    testImplementation("org.testcontainers:testcontainers:1.17.3")
+    testImplementation("com.github.tomakehurst:wiremock-jre8:2.33.2")
     testImplementation("ch.qos.logback:logback-core:1.3.0-alpha14")
-    testImplementation("ch.qos.logback:logback-classic:1.3.0-alpha14")
-    dokkaJavadocPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.10")
+    testImplementation("ch.qos.logback:logback-classic:1.3.0-alpha16")
+    dokkaJavadocPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.7.0")
 }
 
 tasks.processResources {
